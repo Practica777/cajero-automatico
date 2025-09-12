a6cb9a444c5271635120f3e5c1eba857612bbfed
Practica777 <soymorales183@gmail.com>
2025-09-11 19:25:00 -0500

    feat: Añadiendo el menú principal

---
+++ cajero_automatico.py
@@ -0,0 +1,22 @@
+print('\n *Cajero automático *\n')
+
+def crear_cuenta(nombre, apellidos, dni, gmail):
+    # Apartado para crear el usuario.
+    
+    
+
+def iniciar_sesion():
+    
+    
+def menu():
+    # Creando menú:
+    print('\n * Crear cuenta * \n')
+    
+    print('[1]. Crear cuenta')
+    print('[2]. Iniciar sesión')
+    print('[3]. Retirar dinero')
+    print('[4]. Depositar dinero')
+    print('[5]. Transferir dinero')
+    print('[6]. Cambiar contraseña')
+    print('[7]. Salir')
+    
