d4aea71aadcd20c9d79da964c8f24251e7e8e658
Practica777 <soymorales183@gmail.com>
2025-09-11 22:52:42 -0500

    mejorando el código

--- cajero_automatico.py
+++ cajero_automatico.py
@@ -1,17 +1,20 @@
 print('\n *Cajero automático *\n')
 
-def crear_cuenta(nombre, apellidos, dni, gmail):
+def crear_cuenta(nombre, apellidos, dni, gmail, password):
     # Apartado para crear el usuario.
-    lista_nombre = [], lista_apellidos = [], lista_dni = [], lista_gmail = []
-    return lista_nombre.append(nombre), lista_apellidos.append(apellidos), lista_dni.append(dni), lista_gmail.append(gmail)
-    
+    lista_nombre = [], lista_apellidos = [], lista_dni = [], lista_gmail = [], lista_password = []
+    lista_nombre.append(nombre), lista_apellidos.append(apellidos), lista_dni.append(dni), lista_gmail.append(gmail), lista_password.append(password)
+    return lista_nombre, lista_apellidos, lista_dni, lista_gmail, lista_password
     
 
 def iniciar_sesion():
     # Apartado de inicio de sesión
     
+
     
-def menu(opcion):
+    
+    
+def menu():
     # Creando menú:
     print('\n * Crear cuenta * \n')
     
@@ -25,5 +28,18 @@ def menu(opcion):
     
     opcion = input('Escoja una opción: [1][2][3][4][5][6][7]> ')
     
-    
-    
+    if opcion == 1:
+        nombre = input('Nombres: ')
+        apellidos = input('Apellidos: ')
+        dni = input('DNI: ')
+        gmail = input('Gmail: ')
+        password = input('contraseña: ')
+        crear_cuenta(nombre, apellidos, dni, gmail, password)
+        print('\n * Cuenta creada exitosamente * \n')
+        exit
+    elif opcion == 2:
+        
+        
+        
+menu()
+
